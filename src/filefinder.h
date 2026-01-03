3daa9799ebfe8f28d76327637307d44c0f22a9ba
MackValentine <mackvalentine@gmail.com>
2026-01-03 19:34:19 +0100

    Video

--- src/filefinder.h
+++ src/filefinder.h
@@ -170,6 +170,8 @@ namespace FileFinder {
 	 */
 	std::string FindSound(std::string_view name);
 
+	std::string FindMovie(std::string_view name);
+
 	/**
 	 * Finds a font file.
 	 * Searches through the current RPG Maker game and the RTP directories.
